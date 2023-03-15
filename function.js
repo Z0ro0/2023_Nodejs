77f4e7d13f22b0f46133733930347320a212eb7f
Z0ro0 <w2110@e-mirim.hs.kr>
2023-03-15 09:52:27 +0900

    세 가지 함수

---
+++ function.js
@@ -0,0 +1,16 @@
+function add(n1, n2){
+    return n1 + n2;
+}
+
+result = add(1, 2);
+console.log(result);
+
+plus = add;//plus도 add의 역할을 할 수 있음
+result = plus(3,4);
+console.log(result);
+
+sub = function(n1, n2){
+    return n1 - n2;
+}
+result = sub(4, 2);
+console.log(result);
