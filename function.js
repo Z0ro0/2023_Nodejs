f18ffae4d98a6bb955d5ed92c60bc8ffff69660b
Z0ro0 <w2110@e-mirim.hs.kr>
2023-03-15 10:01:11 +0900

    화살표 함수 한 줄로 바꾸기

--- function.js
+++ function.js
@@ -9,8 +9,6 @@ plus = add;//plus도 add의 역할을 할 수 있음
 result = plus(3,4);
 console.log(result);
 
-sub = (n1, n2) => {
-    return n1 - n2;
-}
+sub = (n1, n2) => n1 - n2;
 result = sub(4, 2);
 console.log(result);
